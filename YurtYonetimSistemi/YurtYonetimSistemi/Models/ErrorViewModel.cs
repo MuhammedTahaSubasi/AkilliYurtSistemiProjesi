610155b161263c95e924df50cef0b27b75793e67
MuhammedTahaSubasi <125051024+MuhammedTahaSubasi@users.noreply.github.com>
2025-03-28 01:30:03 +0300

    Initial commit

---
+++ YurtYonetimSistemi/YurtYonetimSistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YurtYonetimSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
